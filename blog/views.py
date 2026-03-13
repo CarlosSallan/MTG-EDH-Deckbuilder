130f89414ea6b9d36c6700c4e6d2ea7dc51e146e
CarlosSallan <karlossallan@gmail.com>
2026-03-13 12:35:14 +0100

    public decks view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+# blog/views.py
 
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Deck
 
+class DeckListView(ListView):
+    model = Deck
+    template_name = "blog/decks.html"
+    context_object_name = "decks"
+
+    def get_queryset(self):
+        return Deck.objects.filter(is_public=True)
 def home(request):
     """Home page view."""
     # request = the incoming HTTP request (URL, method, headers, user info, etc.)
