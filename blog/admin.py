011f315e8ef0d7c79be194a9230c2b3f7cca1336
CarlosSallan <karlossallan@gmail.com>
2026-03-13 12:35:14 +0100

    public decks view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Card, Deck, DeckCard
 
-# Register your models here.
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ("name", "set_code", "collector_number", "scryfall_id")
+    search_fields = ("name", "set_code", "collector_number")
+    list_filter = ("set_code",)
+
+
+class DeckCardInline(admin.TabularInline):
+    model = DeckCard
+    extra = 1
+
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+    list_display = ("name", "author", "commander", "is_public", "card_count", "updated_at")
+    list_filter = ("is_public", "created_at")
+    search_fields = ("name", "author__username")
+    inlines = [DeckCardInline]
+
+
+@admin.register(DeckCard)
+class DeckCardAdmin(admin.ModelAdmin):
+    list_display = ("deck", "card", "quantity", "is_commander")
+    list_filter = ("is_commander",)
+    search_fields = ("deck__name", "card__name")
